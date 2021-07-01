a9a6cd69fc9f5eae7ca8daac952666f6f46843b9
Minh <minhtb1@kaopiz.com>
2021-07-01 13:21:11 +0700

    Display questions

---
+++ resources/views/question/index.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-10">
+            <div class="card">
+                <div class="card-header">{{ __('All Questions') }}</div>
+
+                <div class="card-body">
+                    @foreach ($questions as $question)
+                        <div class="media">
+                            <div class="media-body">
+                                <h3 class="mt-0">{{ $question->title }}</h3>
+                                {{ \Illuminate\Support\Str::limit($question->body, 250) }}
+                            </div>
+                        </div>
+                        <hr>
+                    @endforeach
+                    {{ $questions->links('pagination::bootstrap-4') }}
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
