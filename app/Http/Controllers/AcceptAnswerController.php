0178acd8c002cc52ba565c43d064cd682169105d
Minh <minhtb1@kaopiz.com>
2021-07-16 13:51:31 +0700

    Implement accept the best answer

--- app/Http/Controllers/AcceptAnswerController.php
+++ app/Http/Controllers/AcceptAnswerController.php
@@ -9,6 +9,10 @@ class AcceptAnswerController extends Controller
 {
     public function __invoke(Answer $answer)
     {
-        dd('accept the answer');
+        $this->authorize('accept', $answer);
+
+        $answer->question->acceptBestAnswer($answer);
+        
+        return back();
     }
 }
