ab2b59e7cddab718c92841341019273bcd52dfca
Minh <minhtb1@kaopiz.com>
2021-07-21 12:26:02 +0700

    Tidy up the views

--- resources/views/question/index.blade.php
+++ resources/views/question/index.blade.php
@@ -1,69 +1,31 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-12">
-            <div class="card">
-                <div class="card-header">
-                    <div class="d-flex align-items-center">
-                        <h2>All Questions</h2>
-                        <div class="ml-auto">                        
-                            <a href="{{ route('questions.create') }}" class="btn btn-outline-secondary">Add</a>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">
+                        <div class="d-flex align-items-center">
+                            <h2>All Questions</h2>
+                            <div class="ml-auto">                        
+                                <a href="{{ route('questions.create') }}" class="btn btn-outline-secondary">Add</a>
+                            </div>
                         </div>
                     </div>
-                </div>
 
-                <div class="card-body">
-                    @include('layouts._message')
-                    @forelse ($questions as $question)
-                        <div class="media">
-                            <div class="d-flex flex-column counters">
-                                <div class="vote">
-                                    <strong>{{ $question->votes_count }} </strong>{{ \Illuminate\Support\Str::plural('vote', $question->votes_count) }}
-                                </div>
-                                <div class="status {{ $question->status }}">
-                                    <strong>{{ $question->answers_count }} </strong>{{ \Illuminate\Support\Str::plural('answer', $question->answers_count) }}
-                                </div>
-                                <div class="view">
-                                    {{ $question->views . " " . \Illuminate\Support\Str::plural('view', $question->views) }}
-                                </div>
+                    <div class="card-body">
+                        @include('layouts._message')
+                        @forelse ($questions as $question)
+                            @include('question._excerpt')
+                        @empty
+                            <div class="alert alert-warning">
+                                <strong>Sorry!</strong> There are no question.
                             </div>
-                            <div class="media-body">
-                                <div class="d-flex align-items-center">
-                                    <h3 class="mt-0 w-75"><a href="{{ $question->url }}">{{ $question->title }}</a></h3>
-                                    <div class="ml-auto ">
-                                        @can('update', $question)
-                                            <a href="{{ route('questions.edit', $question->id) }}" class="btn btn-sm btn-outline-info">Edit</a>
-                                        @endcan
-                                        @can('delete', $question)
-                                            <form class="form-delete" method="POST" action="{{ route('questions.destroy', $question->id) }}">
-                                                @method('DELETE')
-                                                @csrf
-                                                <button type="submit" class="btn btn-sm btn-outline-danger" onclick="return confirm('Are you sure?')">Delete</button>
-                                            </form>
-                                        @endcan
-                                    </div>
-                                </div>
-                                <p class="lead">
-                                    Asked by
-                                    <a href="$question->user->url">{{ $question->user->name }}</a>
-                                    <small class="text-muted">{{ $question->created_date }}</small>
-                                </p>
-                                <div class="excerpt">
-                                    {{ $question->excerpt(350) }}
-                                </div>
-                            </div>
-                        </div>
-                        <hr>
-                    @empty
-                        <div class="alert alert-warning">
-                            <strong>Sorry!</strong> There are no question.
-                        </div>
-                    @endforelse
-                    {{ $questions->links('pagination::bootstrap-4') }}
+                        @endforelse
+                        {{ $questions->links('pagination::bootstrap-4') }}
+                </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
